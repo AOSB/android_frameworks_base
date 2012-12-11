8af48a63e3badda0943c6d0f305261bb4f68afcf
jt1134 <jt1134@gmail.com>
2012-12-11 00:03:12 -0600

    LockScreen: allow long-press actions from hardware keys (1/2)
    Allow following actions with button long-presses on lockscreen:
    - toggle flashlight (if available)
    - play/pause/next/previous track selection
    - toggle silent mode
    
    Allow customizing of back, menu, and home buttons. Search
    button events are completely ignored on the lockscreen.
    
    Auto-detect hardware keys, only show appropriate menu
    options based on the actual hardware keys available on the
    device (or completely hide if there are no hardware keys).
    
    Based largely on Danny Baumann's work from gingerbread.
    
    Ported from CM10
    Change-Id: Iff32736d00ca5d4122f64a6a861bbb0a1de9cca9
    
    Fix home/menu unlock with long press hardware key
    - Forward port of commit 2a572ee
    Change-Id: Ic2eb85542f8052fe1662244fe5e72ba12a98608b
    
    Keyguard: Fix for home wake/unlock
    sometimes device is waking and unlocking during the same keypress.
    Change-Id: I72ea7611d39aac0cd03dfd5aa7036aeef7600140
    
    Use sticky broadcast to convey torch state (1/2).
    It doesn't really make sense to store the (non-persistent) state of the
    torch in a (persistent) setting. As the torch is sending a broadcast
    anyway, make that broadcast sticky and use it to convey the state.
    
    Also unify the torch constants (actions, extras, activity names) at a
    common place.
    Change-Id: If845bef27cc79990794aa748dce18cfea3bac1f5
    
    Fix Home/Menu/Back keys' unlock
    Change-Id: Ibd3842b6ad8e037a2a9f8eb581e7c55900017b0f
    
    LockScreen: allow hardware key longpress to put device to sleep
    Change-Id: I54260e21c462bcabd9b6383e22f44d644630b4d3

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3730,6 +3730,24 @@ public final class Settings {
          */
         public static final String HALO_UNLOCK_PING = "halo_unlock_ping";
 
+         /**
+         * Action for long-pressing back button on lock screen
+         * @hide
+         */
+        public static final String LOCKSCREEN_LONG_BACK_ACTION = "lockscreen_long_back_action";
+
+        /**
+         * Action for long-pressing home button on lock screen
+         * @hide
+         */
+        public static final String LOCKSCREEN_LONG_HOME_ACTION = "lockscreen_long_home_action";
+
+        /**
+         * Action for long-pressing menu button on lock screen
+         * @hide
+         */
+        public static final String LOCKSCREEN_LONG_MENU_ACTION = "lockscreen_long_menu_action";
+
         /**
          * Performance profile
          * @see config_perf_profile_prop in frameworks/base/core/res/res/values/config.xml
