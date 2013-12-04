9944c2a6eb40142a430358df0370473d5ec2aec7
cristianomatos <cristianobmatos@gmail.com>
2013-12-04 08:12:31 -0200

    RAM bar (1/2) - This commit includes small re-sizes to fit transparency in new launcher and also moves clear recents button to bottom|left
    Thanks to @kufikugel for hole code ported from 4.3
    
    Change-Id: If1341c1ad5614a1adca07358910a4df85c919966
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/layout-land/status_bar_recent_panel.xml
    	packages/SystemUI/res/layout/status_bar_recent_panel.xml
    
    Conflicts:
    	packages/SystemUI/res/layout-land/status_bar_recent_panel.xml
    	packages/SystemUI/res/layout/status_bar_recent_panel.xml

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2874,6 +2874,36 @@ public final class Settings {
          */
         public static final String NAVIGATION_BAR_WIDTH = "navigation_bar_width";
 
+	/**
+         * whether which Ram Usage Bar mode is used on recent switcher
+         * 0 = none, 1 = only app use, 2 = app and cache use, 3 = app, cache and system use
+         * @hide
+         */
+        public static final String RECENTS_RAM_BAR_MODE = "recents_ram_bar_mode";
+
+        /**
+         * Ram Usage Bar system mem color
+         *
+         * @hide
+         */
+        public static final String RECENTS_RAM_BAR_MEM_COLOR = "recents_ram_bar_mem_color";
+
+        /**
+         * Ram Usage Bar cached mem color
+         *
+         * @hide
+         */
+        public static final String RECENTS_RAM_BAR_CACHE_COLOR = "recents_ram_bar_cache_color";
+
+        /**
+         * Ram Usage Bar app mem color
+         *
+         * @hide
+         */
+        public static final String RECENTS_RAM_BAR_ACTIVE_APPS_COLOR = "recents_ram_bar_active_apps_color";
+
+
+
         /**
         * Notification Power Widget - Custom Brightness Mode
         * @hide
