fd6ce28d79137452545af563f66e03dbff48e611
garwedgess <garethwilliams21@gmail.com>
2013-12-12 21:05:37 +0100

    Notification drawer: full swipe to switch detection (1/2)
    Add an option to detect swipe to switch on full screen, not just the bottom bar
    
    Change-Id: Ib02df40afd95705910955b69645eeb3c49047b18

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3587,6 +3587,13 @@ public final class Settings {
          */
         public static final String PERFORMANCE_PROFILE = "performance_profile";
 
+        /**
+         * Wether to allow full screen swipe detection on notification drawer
+         * for swipe to switch or not
+         * @hide
+         */
+        public static final String SWIPE_TO_SWITCH_SCREEN_DETECTION = "full_swipe_to_switch_detection";
+
 	/**
 	* Navigation bar button color
 	* @hide
