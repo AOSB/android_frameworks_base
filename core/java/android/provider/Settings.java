e531513af306c1be31416dc4edc570126da1e678
0xD34D <clark@scheffsblend.com>
2013-12-13 10:38:37 -0800

    Screen recorder: add ability to record audio from mic [1/2]
    Change-Id: I2422a3c651e469bc9ffeb6b101f43e145dc5de85
    
    Conflicts:
    
    	core/java/android/provider/Settings.java
    
    Change-Id: I2422a3c651e469bc9ffeb6b101f43e145dc5de85

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4207,6 +4207,12 @@ public final class Settings {
         public static final String NOTIFICATION_CUSTOM_CARRIER_LABEL = "notification_custom_carrier_label";
 
         /**
+         * Whether to include audio when recording a video
+         * @hide
+         */
+        public static final String SCREEN_RECORDER_RECORD_AUDIO = "screen_recorder_record_audio";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
