be0fcd2ee070c8e6f31f21ee1dfbc641f83f3b7b
Ryan Hope <rmh3093@gmail.com>
2013-12-19 09:56:36 -0500

    part 1 of LTE toggle support
    Change-Id: I2e1cf08b4a6d49492eec63d4106c857b3905addd

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -514,6 +514,22 @@ public class TelephonyManager {
     private static final String sLteOnCdmaProductType =
         SystemProperties.get(TelephonyProperties.PROPERTY_LTE_ON_CDMA_PRODUCT_TYPE, "");
 
+    public int getPreferredNetworkMode() {
+        try {
+            return getITelephony().getPreferredNetworkMode();
+        } catch (RemoteException ex) {
+            return -1;
+        }
+    }
+
+    public int getLteState() {
+	try {
+	    return getITelephony().getLteState();
+        } catch (RemoteException ex) {
+            return -1;
+        }
+    }
+
     /**
      * Return if the current radio is LTE on CDMA. This
      * is a tri-state return value as for a period of time
@@ -757,9 +773,9 @@ public class TelephonyManager {
     /**
      * {@hide}
      */
-    public void toggleLTE(boolean on) {
+    public void toggleLTE() {
         try {
-            getITelephony().toggleLTE(on);
+            getITelephony().toggleLTE();
         } catch (RemoteException e) {
             //Silently fail
         }
