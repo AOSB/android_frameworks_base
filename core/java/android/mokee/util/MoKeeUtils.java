25b7217aa50330803750946ce42452862099c94b
hany <hany.alsamman@gmail.com>
2013-12-29 07:49:31 +0000

    support chinese language from moKee
    Change-Id: I0b4ef03bf218fa3e996bc2517a6b55b99be75682

---
+++ core/java/android/mokee/util/MoKeeUtils.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2014 The MoKee OpenSource Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.mokee.util;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.Resources;
+import java.text.DecimalFormat;
+import java.util.Locale;
+
+public class MoKeeUtils {
+
+    public static boolean isChineseLanguage() {
+
+       return Resources.getSystem().getConfiguration().locale.getLanguage().startsWith(Locale.CHINESE.getLanguage());
+
+    }
+
+    public static boolean isApkInstalled(String packagename, Context context) {
+        PackageInfo packageInfo;
+
+        try {
+            packageInfo = context.getPackageManager().getPackageInfo(packagename, 0);
+        } catch (NameNotFoundException e) {
+            packageInfo = null;
+        }
+
+        if (packageInfo == null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * Convert filesize unit
+     * 
+     * @param size
+     * @return
+     */
+    public static String formetFileSize(long size) {
+        DecimalFormat df = new DecimalFormat("#0.00");
+        String fileSizeString = "";
+        if (size < 1024) {
+            fileSizeString = df.format((double) size) + "B";
+        } else if (size < 1048576) {
+            fileSizeString = df.format((double) size / 1024) + "K";
+        } else if (size < 1073741824) {
+            fileSizeString = df.format((double) size / 1048576) + "M";
+        } else {
+            fileSizeString = df.format((double) size / 1073741824) + "G";
+        }
+        return fileSizeString;
+    }
+}
