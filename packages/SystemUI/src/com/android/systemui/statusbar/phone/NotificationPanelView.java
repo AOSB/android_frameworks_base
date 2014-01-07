9f2101167266f5d8990f91e972f7fbf35e6ebe40
PrimeDirective <activethrasher00@gmail.com>
2014-01-06 21:59:23 -0500

    notification/settingspanel: stop getting a gesture recorder
    We're currently getting gesture information on fling even
    if the log ouput for DEBUG_GESTURES is turned off so.. lets stop.
    
    -Vanir
    
    Change-Id: I70810fb2fb9e4b134492ffdd1ac844fe65f7fb75

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -96,11 +96,13 @@ public class NotificationPanelView extends PanelView {
 
     @Override
     public void fling(float vel, boolean always) {
-        GestureRecorder gr = ((PhoneStatusBarView) mBar).mBar.getGestureRecorder();
-        if (gr != null) {
-            gr.tag(
-                "fling " + ((vel > 0) ? "open" : "closed"),
-                "notifications,v=" + vel);
+        if (DEBUG_GESTURES) {
+            GestureRecorder gr = ((PhoneStatusBarView) mBar).mBar.getGestureRecorder();
+            if (gr != null ) {
+                gr.tag(
+                    "fling " + ((vel > 0) ? "open" : "closed"),
+                    "notifications,v=" + vel);
+            }
         }
         super.fling(vel, always);
     }
@@ -331,7 +333,7 @@ public class NotificationPanelView extends PanelView {
             if (f !=  null) {
                 Bitmap backgroundBitmap = BitmapFactory.decodeFile(f.getAbsolutePath());
                 mBackgroundDrawable =
-                    new BitmapDrawable(getContext().getResources(), backgroundBitmap);
+                        new BitmapDrawable(getContext().getResources(), backgroundBitmap);
             }
         }
         if (mBackgroundDrawable != null) {
@@ -350,7 +352,7 @@ public class NotificationPanelView extends PanelView {
             if (f !=  null) {
                 Bitmap backgroundBitmap = BitmapFactory.decodeFile(f.getAbsolutePath());
                 mBackgroundDrawableLandscape =
-                    new BitmapDrawable(getContext().getResources(), backgroundBitmap);
+                        new BitmapDrawable(getContext().getResources(), backgroundBitmap);
             }
         }
         if (mBackgroundDrawableLandscape != null) {
