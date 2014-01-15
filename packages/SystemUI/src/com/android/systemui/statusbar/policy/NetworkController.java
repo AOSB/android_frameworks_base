84382cd5c2761b789c24ea4d7d5824ee3eac20c2
Muhammed Nazim <mnm9994u@gmail.com>
2014-01-15 16:35:37 +0100

    WiFi label is not a Quote!
    Cleanup the damn quotes around the WiFi label in the
    notification panel. This only removes the quotes from the first
    and last positions of the string.
    
    PS 2 : WiFi name can have quotes!
    
    Change-Id: I90badb7b2d8160e41da2632dac79042c4be37f2a

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkController.java
@@ -1235,6 +1235,11 @@ public class NetworkController extends BroadcastReceiver implements DemoMode {
             }
         }
 
+        // Cleanup the double quotes
+        if (wifiLabel.length() > 0) {
+            wifiLabel = wifiLabel.replaceAll("^\"|\"$", "");
+        }
+
         final String customLabel = Settings.System.getString(mContext.getContentResolver(),
                 Settings.System.NOTIFICATION_CUSTOM_CARRIER_LABEL);
         if (customLabel != null && customLabel.length() > 0) {
