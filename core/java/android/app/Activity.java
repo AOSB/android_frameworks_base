d182b0f753007dfce67aad3fedd4567d9b3800f6
hany <hany.alsamman@gmail.com>
2014-01-21 13:16:17 +0000

    Revert "HALO: make windows moveable and scaleable"
    This reverts commit 212184dd77052ed2d91b7e3fbd8e6d504fbb2bd1.

--- core/java/android/app/Activity.java
+++ core/java/android/app/Activity.java
@@ -39,7 +39,6 @@ import android.content.res.TypedArray;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
-import android.graphics.Point;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.media.AudioManager;
@@ -2465,9 +2464,6 @@ public class Activity extends ContextThemeWrapper
         return onKeyShortcut(event.getKeyCode(), event);
     }
 
-    boolean scaleW, scaleH, move;
-    Point lastPos;
-
     /**
      * Called to process touch screen events.  You can override this to
      * intercept all touch screen events before they are dispatched to the
@@ -2479,48 +2475,8 @@ public class Activity extends ContextThemeWrapper
      * @return boolean Return true if this event was consumed.
      */
     public boolean dispatchTouchEvent(MotionEvent ev) {
-
-        WindowManager.LayoutParams attrs = mWindow.getAttributes();
-        switch (ev.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                onUserInteraction();
-                if (mWindow.mIsFloatingWindow) {
-                 Log.d(TAG, "Y: " + ev.getY() + " Raw: " + ev.getRawY());
-                    if (ev.getX() >= attrs.width - 50) scaleW = true;
-                    if (ev.getY() >= attrs.height - 50) scaleH = true;
-                    if (ev.getY() <= 50) move = true;
-                    if (move || scaleW || scaleH) {
-                        lastPos = new Point((int)ev.getRawX(), (int)ev.getRawY());
-                        return true;
-                    }
-                }
-                break;
-            case MotionEvent.ACTION_MOVE:
-               if (mWindow.mIsFloatingWindow && lastPos != null) {
-                    int x = attrs.x;
-                    int y = attrs.y;
-                 Point screenSize = new Point();
-                    mWindowManager.getDefaultDisplay().getSize(screenSize);
-                    if (move) {
-                        Log.e(TAG, "Move!");
-                        x = attrs.x + ((int)ev.getRawX() - lastPos.x);
-                        y = attrs.y + ((int)ev.getRawY() - lastPos.y);
-                    }
-                    int width = (int) (scaleW ? attrs.width + ((int)ev.getRawX() - lastPos.x) : attrs.width);
-                    int height = (int) (scaleH ? attrs.height + ((int)ev.getRawY() - lastPos.y) : attrs.height);
-                    mWindow.setLayout(Math.max(0, Math.min(x, screenSize.x - width)), Math.max(0, Math.min(y, screenSize.y - height)),
-                            Math.min(width, screenSize.x), Math.min(height, screenSize.y));
-                    lastPos.x = (int)ev.getRawX();
-                    lastPos.y = (int)ev.getRawY();
-                    return true;
-                }
-                break;
-            case MotionEvent.ACTION_UP:
-                boolean ret = scaleW || scaleH || move;
-                scaleW = scaleH = move = false;
-                lastPos = null;
-                if (ret) return true;
-                break;
+        if (ev.getAction() == MotionEvent.ACTION_DOWN) {
+            onUserInteraction();
         }
 
 	int mHaloEnabled = (Settings.System.getInt(getContentResolver(), Settings.System.HALO_ENABLED, 0));
@@ -5335,19 +5291,17 @@ public class Activity extends ContextThemeWrapper
             // Create our new window
             mWindow = PolicyManager.makeNewWindow(this);
             mWindow.mIsFloatingWindow = true;
-            mWindow.setCloseOnTouchOutsideIfNotSet(false);
-            mWindow.setGravity(Gravity.TOP | Gravity.LEFT);
+            mWindow.setCloseOnTouchOutsideIfNotSet(true);
+            mWindow.setGravity(Gravity.CENTER);
 
-            //if (this instanceof LayerActivity || android.os.Process.myUid() == android.os.Process.SYSTEM_UID) {
-            int flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL;
-                mWindow.setFlags(flags, flags);
+            if (this instanceof LayerActivity || android.os.Process.myUid() == android.os.Process.SYSTEM_UID) {
+                mWindow.setFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND,
+                        WindowManager.LayoutParams.FLAG_DIM_BEHIND);
                 WindowManager.LayoutParams params = mWindow.getAttributes();
                 params.alpha = 1f;
-                //params.dimAmount = 0.25f;
-                //params.x = 10;
-                //params.y = 10;
-                mWindow.setAttributes(params);
-            //}
+                params.dimAmount = 0.25f;
+                mWindow.setAttributes((android.view.WindowManager.LayoutParams) params);
+            }
 
             // Scale it
             scaleFloatingWindow(context);
@@ -5373,12 +5327,11 @@ public class Activity extends ContextThemeWrapper
         Display display = wm.getDefaultDisplay();
         DisplayMetrics metrics = new DisplayMetrics();
         display.getMetrics(metrics);
-        boolean portrait = metrics.heightPixels > metrics.widthPixels;
-        int width = (int)(metrics.widthPixels * (portrait ? 0.9f : 0.7f));
-        int height = (int)(metrics.heightPixels * (portrait ? 0.7f : 0.9f));
-        int x = (metrics.widthPixels - width) / 2;
-        int y = (metrics.heightPixels - height) / 2;
-        mWindow.setLayout(x, y, width, height);
+        if (metrics.heightPixels > metrics.widthPixels) {
+            mWindow.setLayout((int)(metrics.widthPixels * 0.9f), (int)(metrics.heightPixels * 0.7f));
+        } else {
+            mWindow.setLayout((int)(metrics.widthPixels * 0.7f), (int)(metrics.heightPixels * 0.8f));
+        }
     }
 
     /** @hide */
