a06eed162adbd2705e26d76ca8cbc51277b72ab0
Jubakuba <Jubakuba@gmail.com>
2014-01-24 21:53:33 -0500

    Framework: Glowpad Torch (1/3)
    PS2: Torch active? Keep screen on.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2927,6 +2927,12 @@ public final class Settings {
         public static final String GESTURE_ANYWHERE_SHOW_TRIGGER = "gesture_anywhere_show_trigger";
 
         /**
+	* Whether double-tap and hold on the lock glowpad starts the torch
+	* @hide
+	*/
+        public static final String LOCKSCREEN_GLOWPAD_TORCH = "lockscreen_glowpad_torch";
+
+        /**
          * @deprecated Use {@link android.provider.Settings.Global#LOW_BATTERY_SOUND}
          * instead
          * @hide
