6987e888f8d22a818ff34bbf1aa4d69957068e1b
Steve Spear <steve.spear@openpeak.com>
2014-02-02 00:31:09 -0500

    Make Navring available to devices with no navbar
    Swipe up from bottom of screen to access navring.
    
    Change-Id: Ib29b37f26187a65faef17571328cef57f6896b29
    Signed-off-by: hany <hany.alsamman@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -22,10 +22,10 @@ import static android.app.StatusBarManager.NAVIGATION_HINT_BACK_ALT;
 import static android.app.StatusBarManager.WINDOW_STATE_HIDDEN;
 import static android.app.StatusBarManager.WINDOW_STATE_SHOWING;
 import static android.app.StatusBarManager.windowStateToString;
+import static com.android.systemui.statusbar.phone.BarTransitions.MODE_LIGHTS_OUT;
 import static com.android.systemui.statusbar.phone.BarTransitions.MODE_OPAQUE;
 import static com.android.systemui.statusbar.phone.BarTransitions.MODE_SEMI_TRANSPARENT;
 import static com.android.systemui.statusbar.phone.BarTransitions.MODE_TRANSLUCENT;
-import static com.android.systemui.statusbar.phone.BarTransitions.MODE_LIGHTS_OUT;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
@@ -131,6 +131,7 @@ import com.android.systemui.statusbar.policy.NetworkController;
 import com.android.systemui.statusbar.policy.NotificationRowLayout;
 import com.android.systemui.statusbar.policy.OnSizeChangedListener;
 import com.android.systemui.statusbar.policy.RotationLockController;
+import com.android.systemui.aokp.SearchPanelSwipeView;
 import com.android.systemui.omni.StatusHeaderMachine;
 
 import java.io.FileDescriptor;
@@ -480,6 +481,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private int mStatusBarMode;
     private int mNavigationBarMode;
     private Boolean mScreenOn;
+    private SearchPanelSwipeView mSearchPanelSwipeView;
 
     private final Runnable mAutohide = new Runnable() {
         @Override
@@ -667,6 +669,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                         checkUserAutohide(v, event);
                         return false;
                     }});
+            } else if (!showNav) {
+                mSearchPanelSwipeView = new SearchPanelSwipeView(mContext, this);
+                mWindowManager.addView(mSearchPanelSwipeView, mSearchPanelSwipeView.getGesturePanelLayoutParams());
+                updateSearchPanel();
             }
         } catch (RemoteException ex) {
             // no window manager? good luck with that
@@ -1099,6 +1105,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         if (mNavigationBarView != null) {
             mNavigationBarView.setDelegateView(mSearchPanelView);
         }
+        if (mSearchPanelSwipeView != null) {
+            mSearchPanelSwipeView.setDelegateView(mSearchPanelView);
+        }
     }
 
     @Override
@@ -1226,6 +1235,13 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         prepareNavigationBarView();
 
         mWindowManager.updateViewLayout(mNavigationBarView, getNavigationBarLayoutParams());
+    }
+
+    private void repositionSearchPanelSwipeView() {
+        if (mSearchPanelSwipeView == null || !mSearchPanelSwipeView.isAttachedToWindow()) return;
+        mSearchPanelSwipeView.updateLayout();
+        mWindowManager.updateViewLayout(mSearchPanelSwipeView, mSearchPanelSwipeView.getGesturePanelLayoutParams());
+        updateSearchPanel();
     }
 
     private void notifyNavigationBarScreenOn(boolean screenOn) {
@@ -3199,6 +3215,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         updateResources();
         repositionNavigationBar();
+        repositionSearchPanelSwipeView();
         updateExpandedViewPos(EXPANDED_LEAVE_ALONE);
         updateShowSearchHoldoff();
     }
@@ -3281,8 +3298,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             mSignalClusterView.setStyle(signalStyle);
             mSignalTextView.setStyle(signalStyle);
         }
-        final ContentResolver cr = mContext.getContentResolver();
-
         //Default to mWindowManagerService.hasNavigationBar()
         boolean hasNav = true; // If below fails then better show the navbar
         try {
@@ -3291,13 +3306,13 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         catch (RemoteException ex) {
             //OH NO!
         }
-        mWantsNavigationBar = Settings.System.getBoolean(cr, Settings.System.ENABLE_NAVIGATION_BAR, hasNav);
+        mWantsNavigationBar = Settings.System.getBoolean(resolver, Settings.System.ENABLE_NAVIGATION_BAR, hasNav);
     }
 
     private void toggleNavigationBar(boolean show) {
         if (show) {
             if (mNavigationBarView != null || mRecreating) return;
-            if (DEBUG) Log.d(TAG, "Enabling navigation bar now");
+           if (DEBUG) Log.d(TAG, "Enabling navigation bar now");
             mNavigationBarView = (NavigationBarView) View.inflate(mContext, R.layout.navigation_bar, null);
             mNavigationBarView.setDisabledFlags(mDisabled);
             mNavigationBarView.setBar(this);
@@ -3697,7 +3712,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         @Override
         public void onChange(boolean selfChange, Uri uri) {
-            updateSettings();
             toggleNavigationBar(mWantsNavigationBar);
             if (uri.equals(Settings.System.getUriFor(
                     Settings.System.QUICK_SETTINGS_TILES))
