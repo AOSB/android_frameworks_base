6612b77fc7fc46f7646086b3d02a05945c0c22cd
hany <hany.alsamman@gmail.com>
2014-02-19 05:30:17 +0000

    Port Base: Battery Saver mode [1/3]
    introduce battery saver mode
    this based from what user want or what user do to saving the battery life
    this include:
    1. Automatically switch mobile network into what user choice and also disable mobile data IF wifi connected to an AP
    2. Automatically switch mobile network and disable mobile data IF battery low (battery level can configure)
    3. Reduce screen brightness based low battery (this feature will disabled if Auto brightness mode enabled)
    4. Not changing network IF on Call or Usb tether enabled or WifiAP enabled
    5. Not changing network IF there is Download/upload progress
    6. Support For Both GSM and CDMA devices (need someone to test for CDMA devices)
    7. every condition has an option :D
    8. Restore all network configuration if Smart Radio disabled
    9. Add Time Range to start Smart Radio
    
    PS2: Ditched brightness change since it`s useless and added data
    and WI-FI on/off
    
    PS3: better handling of boot process , fix bracket derps
    
    PS4: 4G options should work now, thanks to Yanuar Harry
    
    pulled from VanirAOSP
    
    Signed-off-by: hany <hany.alsamman@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/BatteryController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/BatteryController.java
@@ -76,7 +76,11 @@ public class BatteryController extends BroadcastReceiver {
     // For HALO
     public void removeStateChangedCallbackHalo(BatteryStateChangeCallbackHalo cb_Halo) {
         mChangeCallbacksHalo.remove(cb_Halo);
-    }  
+    }
+
+    public void unregisterController(Context context) {	
+        context.unregisterReceiver(this);
+    }
 
     public void onReceive(Context context, Intent intent) {
         final String action = intent.getAction();
