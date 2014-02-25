d840ad0426c7446765b0a1a912c7914a9d412f74
hany <hany.alsamman@gmail.com>
2014-02-25 05:24:33 +0000

    Enable Navigation bar for devices new way
    based on :
    https://github.com/VanirAOSP/frameworks_base/commit/5583cd9ab2b8b3d89b777c129c5b4f3a43f7cdbe#diff-6b00ad442e3fbcaca832effef4f6bde3R10240
    
    thanks a ton and credit for @nuclearmistake!
    
    Signed-off-by: hany <hany.alsamman@gmail.com>

--- services/java/com/android/server/wm/WindowManagerService.java
+++ services/java/com/android/server/wm/WindowManagerService.java
@@ -10277,6 +10277,11 @@ public class WindowManagerService extends IWindowManager.Stub
     }
 
     @Override
+    public boolean wantsNavigationBar() {
+        return mPolicy.wantsNavigationBar();
+    }
+
+    @Override
     public void lockNow(Bundle options) {
         mPolicy.lockNow(options);
     }
