62cb4b75d3c1e865ec656d18085c0c5aac534096
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2014-03-07 17:11:02 -0500

    Add people lookup settings keys
    Change-Id: I2ff9bb7684d04f4f9b640939c7db269873aa3308

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3850,17 +3850,24 @@ public final class Settings {
         public static final String QUIET_HOURS_DIM = "quiet_hours_dim";
 
         /**
-	      * Enable looking up of phone numbers of nearby places
-	      *
-	      * @hide
-	      */
+         * Enable looking up of phone numbers of nearby places
+         *
+         * @hide
+         */
         public static final String ENABLE_FORWARD_LOOKUP = "enable_forward_lookup";
 
         /**
-	      * Enable looking up of information of phone numbers not in the contacts
-	      *
-	      * @hide
-	      */
+	* Enable looking up of phone numbers of people
+	*
+	* @hide
+	*/
+        public static final String ENABLE_PEOPLE_LOOKUP = "enable_people_lookup";
+
+        /**
+         * Enable looking up of information of phone numbers not in the contacts
+         *
+         * @hide
+         */
         public static final String ENABLE_REVERSE_LOOKUP = "enable_reverse_lookup";
 
         /**
@@ -3869,6 +3876,13 @@ public final class Settings {
         * @hide
         */
         public static final String FORWARD_LOOKUP_PROVIDER = "forward_lookup_provider";
+
+        /**
+	* The people lookup provider
+	*
+	* @hide
+	*/
+        public static final String PEOPLE_LOOKUP_PROVIDER = "people_lookup_provider";
 
         /**
         * The reverse lookup provider
