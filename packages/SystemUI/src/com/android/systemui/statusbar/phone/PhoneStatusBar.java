0d4eb145b4f7f7e0539692d357162c46fe106b57
hany <hany.alsamman@gmail.com>
2014-03-08 09:25:49 +0000

    Revert "Navring: Make it optional [1/2]"
    This reverts commit c2e539ec8813f7cea3dbe88eaac445cd8dc43f70.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -423,14 +423,11 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
     private void updateNavigationBarState() {
         boolean showNav = true;
-	boolean showNavRing = false;
         try {
               showNav = mWindowManagerService.hasNavigationBar()
                                   || mWindowManagerService.wantsNavigationBar();
-              showNavRing = Settings.System.getBoolean(mContext.getContentResolver(), 
-				Settings.System.ENABLE_NAVRING_SWIPE, false);
         } catch(RemoteException ex) {
-            Log.e("NavBar", "Exception while checking Navigation Bar||Ring stuff via WindowManagerStuff", ex);
+            Log.e("NavBar", "Exception while checking NavigationBar stuff via WindowManagerStuff", ex);
         }
 
         if (DEBUG) Log.v(TAG, "hasNavigationBar=" + showNav);
@@ -468,9 +465,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 }
                 mNavigationBarView = null;
             }
-            if (mSearchPanelSwipeView == null && showNavRing) {
+            if (mSearchPanelSwipeView == null) {
                 mSearchPanelSwipeView = new SearchPanelSwipeView(mContext, this);
-	    }
+            }
             try {
                 mWindowManager.addView(mSearchPanelSwipeView, mSearchPanelSwipeView.getGesturePanelLayoutParams());
             } catch (java.lang.IllegalStateException ex) {
