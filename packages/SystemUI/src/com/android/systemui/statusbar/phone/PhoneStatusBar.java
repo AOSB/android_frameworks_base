c89631461e24c25ce19093076c9ef97b7fc554d8
BMc08GT <brandon.mcansh@gmail.com>
2014-03-12 03:53:49 -0400

    Enable nav bar for devices with hardware keys
    Change-Id: I2704e99571deff62523da8113971096f79d14fc9
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>
    Signed-off-by: hany <hany.alsamman@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -89,6 +89,7 @@ import android.view.ViewGroup.LayoutParams;
 import android.view.ViewPropertyAnimator;
 import android.view.ViewStub;
 import android.view.WindowManager;
+import android.view.WindowManagerGlobal;
 import android.view.animation.AccelerateInterpolator;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -195,6 +196,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                                                     // faster than mSelfCollapseVelocityPx)
 
     PhoneStatusBarPolicy mIconPolicy;
+
+    private boolean mWantsNavigationBar = false;
 
     // These are no longer handled by the policy, because we need custom strategies for them
     BluetoothController mBluetoothController;
@@ -415,12 +418,15 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     Settings.System.STATUS_BAR_CUSTOM_HEADER), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.CUSTOM_RECENT), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.ENABLE_NAVIGATION_BAR), false, this);
             updateSettings();
         }
 
         @Override
         public void onChange(boolean selfChange) {
             updateSettings();
+            toggleNavigationBar(mWantsNavigationBar);
         }
     }
 
@@ -656,7 +662,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         updateShowSearchHoldoff();
 
         try {
-            boolean showNav = mWindowManagerService.hasNavigationBar();
+            boolean showNav = mWindowManagerService.hasNavigationBar()
+                              || mWindowManagerService.wantsNavigationBar();
             if (DEBUG) Log.v(TAG, "hasNavigationBar=" + showNav);
             if (showNav && !mRecreating) {
                 mNavigationBarView =
@@ -3284,6 +3291,33 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             mSignalClusterView.setStyle(signalStyle);
             mSignalTextView.setStyle(signalStyle);
         }
+        final ContentResolver cr = mContext.getContentResolver();
+
+        //Default to mWindowManagerService.hasNavigationBar()
+        boolean hasNav = true; // If below fails then better show the navbar
+        try {
+            hasNav = mWindowManagerService.hasNavigationBar();
+        }
+        catch (RemoteException ex) {
+            //OH NO!
+        }
+        mWantsNavigationBar = Settings.System.getBoolean(cr, Settings.System.ENABLE_NAVIGATION_BAR, hasNav);
+    }
+
+    private void toggleNavigationBar(boolean show) {
+        if (show) {
+            if (mNavigationBarView != null || mRecreating) return;
+            if (DEBUG) Log.d(TAG, "Enabling navigation bar now");
+            mNavigationBarView = (NavigationBarView) View.inflate(mContext, R.layout.navigation_bar, null);
+            mNavigationBarView.setDisabledFlags(mDisabled);
+            mNavigationBarView.setBar(this);
+            addNavigationBar();
+        } else {
+            if (mNavigationBarView == null) return;
+            if (DEBUG) Log.d(TAG, "Disabling navigation bar now");
+            mWindowManager.removeView(mNavigationBarView);
+            mNavigationBarView = null;
+        }
     }
 
     private void resetUserSetupObserver() {
@@ -3691,6 +3725,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         @Override
         public void onChange(boolean selfChange, Uri uri) {
+            updateSettings();
+            toggleNavigationBar(mWantsNavigationBar);
             if (uri.equals(Settings.System.getUriFor(
                     Settings.System.QUICK_SETTINGS_TILES))
                 || uri.equals(Settings.System.getUriFor(
