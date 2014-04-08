95fd316801f496654115ca777f1d73e627d08492
Les Prock <les@cyngn.com>
2014-04-07 17:53:26 -0700

    Wallpaper Overscroll and Transparent NavBar
    Now possible to offset wallpaper windows by more than the size of the wallpaper
    and add added a transparent mode to the NavigationBar
    
    Change-Id: I2b0f1129a4cf73b8def35abb7072d0472a9cc972

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -25,15 +25,20 @@ import android.app.ActivityManagerNative;
 import android.app.StatusBarManager;
 import android.app.admin.DevicePolicyManager;
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.database.ContentObserver;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
+import android.os.Bundle;
 import android.net.Uri;
 import android.os.Handler;
 import android.os.Message;
@@ -128,6 +133,9 @@ public class NavigationBarView extends LinearLayout {
 
     // performs manual animation in sync with layout transitions
     private final NavTransitionListener mTransitionListener = new NavTransitionListener();
+
+    private boolean mModLockDisabled = true;
+    private SettingsObserver mObserver;
 
     private class NavTransitionListener implements TransitionListener {
         private boolean mBackTransitioning;
@@ -264,6 +272,8 @@ public class NavigationBarView extends LinearLayout {
         watchForDevicePolicyChanges();
 
         mLockUtils = new LockPatternUtils(context);
+
+        mObserver = new SettingsObserver(new Handler());
     }
 
     private void watchForDevicePolicyChanges() {
@@ -469,7 +479,7 @@ public class NavigationBarView extends LinearLayout {
                 Settings.System.getInt(mContext.getContentResolver(),
                         Settings.System.LOCKSCREEN_NOTIFICATIONS_PRIVACY_MODE, 0) == 0;
         setVisibleOrGone(getSearchLight(), showSearch);
-        setVisibleOrGone(getCameraButton(), showCamera);
+        setVisibleOrGone(getCameraButton(), showCamera && mModLockDisabled);
         setVisibleOrGone(getNotifsButton(), showNotifs && mWasNotifsButtonVisible);
 
         mBarTransitions.applyBackButtonQuiescentAlpha(mBarTransitions.getMode(), true /*animate*/);
@@ -713,6 +723,24 @@ public class NavigationBarView extends LinearLayout {
         invalidate();
     }
 
+    @Override
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+
+        final Bundle keyguard_metadata = NavigationBarView
+                    .getApplicationMetadata(mContext, "com.android.keyguard");
+                if (null != keyguard_metadata &&
+                    keyguard_metadata.getBoolean("com.cyanogenmod.keyguard", false)) {
+                        mObserver.observe();
+                }
+    }
+
+    @Override
+    public void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        mObserver.unobserve();
+    }
+
     private void watchForAccessibilityChanges() {
         final AccessibilityManager am =
                 (AccessibilityManager) mContext.getSystemService(Context.ACCESSIBILITY_SERVICE);
@@ -935,6 +963,54 @@ public class NavigationBarView extends LinearLayout {
             }
         }
         pw.println();
+    }
+
+    private static Bundle getApplicationMetadata(Context context, String pkg) {
+        if (pkg != null) {
+            try {
+                ApplicationInfo ai = context.getPackageManager().
+                    getApplicationInfo(pkg, PackageManager.GET_META_DATA);
+                return ai.metaData;
+            } catch (NameNotFoundException e) {
+                return null;
+            }
+        }
+
+        return null;
+    }
+
+    private class SettingsObserver extends ContentObserver {
+            private boolean mObserving = false;
+
+            SettingsObserver(Handler handler) {
+                super(handler);
+            }
+
+            void observe() {
+                mObserving = true;
+                ContentResolver resolver = mContext.getContentResolver();
+                resolver.registerContentObserver(
+                    Settings.System.getUriFor(Settings.System.LOCKSCREEN_MODLOCK_ENABLED),
+                    false, this);
+
+                // intialize mModlockDisabled
+                onChange(false);
+            }
+
+            void unobserve() {
+                if (mObserving) {
+                    mContext.getContentResolver().unregisterContentObserver(this);
+                    mObserving = false;
+                }
+            }
+
+            @Override
+            public void onChange(boolean selfChange) {
+                mModLockDisabled = Settings.System.getInt(mContext.getContentResolver(),
+                        Settings.System.LOCKSCREEN_MODLOCK_ENABLED, 1) == 0;
+                setDisabledFlags(mDisabledFlags, true /* force */);
+            }
+        }
     }
 
     private void addSeparator(LinearLayout layout, boolean landscape, int size, float weight) {
