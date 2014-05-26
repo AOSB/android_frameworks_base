691e67c62a4276957e84262c10f7369a2396457c
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2014-05-27 00:30:39 +0100

    wm: Underp menu key release processing
    Change Iacfedf711d43f6637023c87c00e6c6b8b8a437dc made key release perform
    the assigned action regardless of alternate modes, but the keypress
    processing depended on said modes. Make it consistent.
    
    Change-Id: I4be44b065f775f0304aa9770778c8fee82ef8ef1

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -2726,7 +2726,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     } else if (mLongPressOnMenuBehavior != KEY_ACTION_NOTHING) {
                         return -1;
                     }
-                } else if (mLongPressOnMenuBehavior != KEY_ACTION_NOTHING) {
+                } else {
                     return -1;
                 }
             }
