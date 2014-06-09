f0626c0fa9ce6a696bbe0e817a9152dc00749fe1
XjCook <xjcook@gmail.com>
2014-06-09 17:08:45 -0400

    HALO: Add custom color [1/2]
    Change-Id: I6b8a3e8b1057e466b947da63e5faf67560781837
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4262,6 +4262,12 @@ public final class Settings {
         public static final String HALO_ENABLED = "halo_enabled";
 
         /**
+	* HALO color, default is 0xff33b5e5 (normal)
+	* @hide
+	*/
+        public static final String HALO_COLOR = "halo_color";
+
+        /**
          * HALO, should default to 0 (no, do not show)
          * @hide
          */
