9d07cfc79965e01f805fad4c12cd958dd112d5ac
Michael Bestas <mikeioannina@gmail.com>
2014-06-11 21:23:27 +0300

    Remove lockscreen slide delay option (2/2)
    Also enable this option for insecure lockscreen:
    * Power button instantly locks
    
    Change-Id: Ibf93519637e0c08bad5d2224c3f76c99b0af60f7

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3598,27 +3598,6 @@ public final class Settings {
         public static final String STATUS_BAR_NETWORK_STATS_UPDATE_INTERVAL = "status_bar_network_stats_update_frequency";
 
         /**
-         * Whether to use a separate delay for "slide to unlock" and security
-         * lock
-         * @hide
-         */
-        public static final String SCREEN_LOCK_SLIDE_DELAY_TOGGLE = "screen_lock_slide_delay_toggle";
-
-        /**
-         * How many ms to delay before enabling the "slide to unlock" screen
-         * lock when the screen goes off due to timeout
-         * @hide
-         */
-        public static final String SCREEN_LOCK_SLIDE_TIMEOUT_DELAY = "screen_lock_slide_timeout_delay";
-
-        /**
-         * How many ms to delay before enabling the "slide to unlock" screen
-         * lock when the screen is turned off by the user
-         * @hide
-         */
-        public static final String SCREEN_LOCK_SLIDE_SCREENOFF_DELAY = "screen_lock_slide_screenoff_delay";
-
-        /**
          * Whether to use the custom quick unlock screen control
          * @hide
          */
