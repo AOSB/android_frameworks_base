6c20d24ae70e03adb83ceb30afc95d6ddb27f4ba
Carlo Savignano <stevewatersy@gmail.com>
2014-06-11 15:06:19 +0200

    HOVER: Fix a couple of things:
    - Handle Ticker properly. Update mTicking, call related statements
      and disable ticker for blacklisted apps only when hover is off.
    - Remove a bad patch when you release your finger, there is no need and
      along with showCurrentNotification() call at setNotification() can even
      be a bad call.
    - Notification removal is already done at BaseStatusBar. Remove useless code.
    
    AICPfy
    Fix statusbar not updating (ticker issue) with halo enabled
    
    Change-Id: Ia0c55ad6c82b05bbf1933f748178ff7a2981bb4c
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2850,10 +2850,12 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                                 | StatusBarManager.DISABLE_NOTIFICATION_TICKER))) {
                     boolean blacklisted = false;
                     // don't pass notifications that run in Hover to Ticker
-                    try {
-                        blacklisted = getNotificationManager().isPackageAllowedForHover(n.getPackageName());
-                    } catch (android.os.RemoteException ex) {
-                        // System is dead
+                    if (mHoverState == HOVER_ENABLED) {
+                        try {
+                            blacklisted = getNotificationManager().isPackageAllowedForHover(n.getPackageName());
+                        } catch (android.os.RemoteException ex) {
+                            // System is dead
+                        }
                     }
                     if (!blacklisted) mTicker.addEntry(n);
                 }
@@ -2876,13 +2878,15 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     }
 
     private class MyTicker extends Ticker {
+        private boolean hasTicked = false;
+
         MyTicker(Context context, View sb) {
             super(context, sb);
         }
 
         @Override
         public void tickerStarting() {
-	    if (!mHaloActive) {
+	    if (!mHaloActive || mHoverState == HOVER_DISABLED) {
                 mTicking = true;
                 mStatusBarContents.setVisibility(View.GONE);
 		mCenterClockLayout.setVisibility(View.GONE);
@@ -2896,7 +2900,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         @Override
         public void tickerDone() {
-	    if (!mHaloActive) {
+	    if (!mHaloActive || mHoverState == HOVER_DISABLED) {
                 mStatusBarContents.setVisibility(View.VISIBLE);
 		mCenterClockLayout.setVisibility(View.VISIBLE);
                 mTickerView.setVisibility(View.GONE);
@@ -2908,7 +2912,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
 
         public void tickerHalting() {
-	    if (!mHaloActive) {
+	    if (!mHaloActive || mHoverState == HOVER_DISABLED) {
                 if (mStatusBarContents.getVisibility() != View.VISIBLE) {
                     mStatusBarContents.setVisibility(View.VISIBLE);
 		    mCenterClockLayout.setVisibility(View.VISIBLE);
