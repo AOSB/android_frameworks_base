90d06ca05b186759d598a55c6ca9721e541c2ce6
Jorge Ruesga <jorge@ruesga.com>
2014-06-19 18:33:16 +0200

    base: don't show global actions if shutdown sequence was started
    Change-Id: Icd77aa7529faf438dcc80e45434826465a11aab8
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- services/java/com/android/server/power/ShutdownThread.java
+++ services/java/com/android/server/power/ShutdownThread.java
@@ -97,6 +97,12 @@ public final class ShutdownThread extends Thread {
     private ShutdownThread() {
     }
 
+    public static boolean isStarted() {
+        synchronized (sIsStartedGuard) {
+            return sIsStarted;
+        }
+    }
+
     /**
      * Request a clean shutdown, waiting for subsystems to clean up their
      * state etc.  Must be called from a Looper thread in which its UI
