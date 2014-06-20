14d8d976eaa53c1574eb2732c801516ca7cb5518
Randall Rushing <randall.rushing@gmail.com>
2014-06-20 01:04:56 -0700

    FastChargeTile: override write method for some devices
    Some device, like m8, require a line break when writing to
    sysfs. Detect m8 (or other devices we can add if need be)
    and override write method if needed
    
    PS1: line breaks and imports
    PS2: change methods
    
    Change-Id: I3eaa534dc1b3cf33b8a29b8bc2b0228d65997ecd
    Signed-off-by: hany <hany.alsamman@gmail.com>

--- packages/SystemUI/src/com/android/systemui/quicksettings/FastChargeTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/FastChargeTile.java
@@ -1,13 +1,19 @@
 package com.android.systemui.quicksettings;
 
 import android.content.Context;
+import android.os.SystemProperties;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import com.android.systemui.R;
 import com.android.systemui.statusbar.phone.QuickSettingsController;
 
 public class FastChargeTile extends FileObserverTile {
 	protected static String TAG = FastChargeTile.class.getSimpleName();
-    public static final String FFC_PATH = "/sys/kernel/fast_charge/force_fast_charge";
+	public static final String FFC_PATH = "/sys/kernel/fast_charge/force_fast_charge";
 
 	public FastChargeTile(Context context, QuickSettingsController qsc) {
 		super(context, qsc);
@@ -19,10 +25,33 @@ public class FastChargeTile extends FileObserverTile {
 	}
 
 	@Override
+	void onPostCreate() {
+		String device = SystemProperties.get("ro.product.device", "none");
+		mNeedsWriteOverride = "m8".equals(device);
+		super.onPostCreate();
+	}
+
+	@Override
 	protected TwoStateTileRes getTileRes() {
 		return new TwoStateTileRes(R.string.quick_settings_fcharge_on_label
 				,R.string.quick_settings_fcharge_off_label
 				,R.drawable.ic_qs_fcharge_on
 				,R.drawable.ic_qs_fcharge_off);
+	}
+
+	protected void setEnabled(boolean enabled) {
+		if (mNeedsWriteOverride) {
+		    try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(new File(FFC_PATH)));
+			String output = "" + (enabled ? "1\n" : "0\n");
+			writer.write(output.toCharArray(), 0, output.toCharArray().length);
+			writer.close();
+		    } catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		    }
+		} else {
+		    super.setEnabled(enabled);
+		}
 	}
 }
