cc813073b894f99506feb19be76e01979749a306
k2wl <kar2wlan@gmail.com>
2014-06-23 22:01:24 -0400

    SystemUI: Expose heads up.
    Signed-off-by: Roman Birg <roman@cyngn.com>
    Signed-off-by: Adnan Begovic <adnan@cyngn.com>
    Change-Id: I276cb1c80d392b7dbac28e5f553207415b558481
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/tv/TvStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/tv/TvStatusBar.java
@@ -161,6 +161,10 @@ public class TvStatusBar extends BaseStatusBar {
     }
 
     @Override
+    public void hideHeadsUp() {
+    }
+
+    @Override
     public void animateExpandSettingsPanel() {
     }
 
