e898f91427fcc2ea893350fa39e1128398128886
hany <hany.alsamman@gmail.com>
2014-09-04 23:00:54 +0000

    Revert "SystemUI: Improve HeadsUp expanding performance."
    This reverts commit e57896a0737a9602fd81a7be8a79b29510a04643.

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/HeadsUpNotificationView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/HeadsUpNotificationView.java
@@ -207,11 +207,6 @@ public class HeadsUpNotificationView extends FrameLayout implements SwipeHelper.
     public void setUserLockedChild(View v, boolean userLocked) {
         if (mHeadsUp != null && mHeadsUp.row == v) {
             mHeadsUp.row.setUserLocked(userLocked);
-
-            setMinimumHeight(userLocked
-                    ? (int) mExpandHelper.getNaturalHeight() + mContentHolder.getPaddingTop()
-                            + mContentHolder.getPaddingBottom()
-                    : 0);
         }
     }
 
