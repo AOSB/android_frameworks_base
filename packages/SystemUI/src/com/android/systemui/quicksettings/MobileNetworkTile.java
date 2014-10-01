aae4f6c8f2e06240a2ee4a6ae1ce2079878dab84
Arnav Gosain <gosain.arnav@gmail.com>
2014-10-01 16:15:55 -0400

    Missing import
    Change-Id: I93f9ecde4b5d384c5be0ad5e29888c95c39c4df1

--- packages/SystemUI/src/com/android/systemui/quicksettings/MobileNetworkTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/MobileNetworkTile.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.view.View.OnLongClickListener;
 import android.view.ViewGroup.MarginLayoutParams;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.android.systemui.R;
 import com.android.systemui.statusbar.phone.QuickSettingsController;
