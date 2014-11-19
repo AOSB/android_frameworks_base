1c98a00db6286f8d6321443ab08aafa6440f72af
DvTonder <david.vantonder@gmail.com>
2014-11-19 18:28:43 -0500

    Fix battery % always showing at < 100%
    Change-Id: I9ce697e824e1f85becfd0e53519d3ae36fc65758
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2970,6 +2970,12 @@ public final class Settings {
 	*/
         public static final String PROGRESSBAR_COLOR_4 = "progressbar_color_4";
 
+         /**
+         * Status bar battery %
+         * @hide
+         */
+        public static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
@@ -3042,6 +3048,7 @@ public final class Settings {
             PHONE_BLACKLIST_PRIVATE_NUMBER_MODE,
             PHONE_BLACKLIST_UNKNOWN_NUMBER_MODE,
             PHONE_BLACKLIST_REGEX_ENABLED,
+            STATUS_BAR_SHOW_BATTERY_PERCENT,
         };
 
         /**
@@ -5177,7 +5184,7 @@ public final class Settings {
          */
         public static final String SLEEP_TIMEOUT = "sleep_timeout";
 
-         /** Protected Components
+        /** Protected Components
          * @hide
          */
         public static final String PROTECTED_COMPONENTS = "protected_components";
